ff8232f5f48014ee43c4449887e4465983a7b6f3
Bonaventura Sanjaya <bonaventura.sanjaya@dexagroup.com>
2024-02-13 10:22:11 +0700

    rfctr general layout

--- src/layouts/GeneralLayout.tsx/index.tsx
+++ src/layouts/GeneralLayout.tsx/index.tsx
@@ -1,6 +1,8 @@
 import { Layout } from "antd";
-import { Footer, Header } from "components";
 import { Outlet } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const { Content } = Layout;
 
